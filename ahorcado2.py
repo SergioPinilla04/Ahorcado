c051d6823e0fec5216c8aadb46589f452e3b262d
Sergio Pinilla <113448338+SergioPinilla04@users.noreply.github.com>
2023-12-29 13:14:17 +0100

    ahorcados 2 y 3

--- ahorcado2.py
+++ ahorcado2.py
@@ -1,10 +1,11 @@
 palabra = input("¡Comienza el juego del ahorcado! Para comenzar, inserte la palabra que se va a adivinar: ")
 letra = input("¡Pruebe con una letra!: ")
 posicion = 0
+encontrada = False
 for caracter in palabra:
     posicion = posicion + 1
-    if letra in palabra:
-        if caracter == letra:
-            print("La letra ", letra, " se encuentra en la posición número ", posicion, " de la palabra objetivo")
-    else:
-        print("¡Fallaste! La letra ", letra, " no está en la palabra que buscas")
+    if caracter == letra:
+        print("La letra ", letra, " se encuentra en la posición número ", posicion, " de la palabra objetivo")
+        encontrada = True
+if not encontrada:
+    print("¡Fallaste! La letra ", letra, " no está en la palabra que buscas")
---
+++ ahorcado3.py
@@ -0,0 +1,20 @@
+palabra = input("¡Comienza el juego del ahorcado! Para comenzar, inserte la palabra que se va a adivinar: ")
+fallos_maximos = 5
+fallos = 0
+
+while fallos < fallos_maximos:
+    letra = input("¡Pruebe con una letra!: ")
+    posicion = 0
+    encontrada = False
+
+    for caracter in palabra:
+        posicion = posicion + 1
+        if caracter == letra:
+            print("La letra ", letra, " se encuentra en la posición número ", posicion, " de la palabra objetivo")
+            encontrada = True
+
+    if not encontrada:
+        fallos += 1
+        print("¡Fallaste! La letra ", letra, " no está en la palabra que buscas. Llevas ", fallos, " fallos.")
+
+print("Has alcanzado el límite de fallos. ¡Fin del juego!")
